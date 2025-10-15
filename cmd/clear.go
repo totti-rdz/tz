3022a77fd4a9b64e8f50edbfb02887ff825bac9f
Totti Rdz <thorsten_herfurtner@gmx.de>
2025-10-16 01:11:23 +0200

    feat: add -a flag to clear command to remove lock files
    Add --all/-a flag that removes common lock files (package-lock.json, yarn.lock, pnpm-lock.yaml, bun.lockb, Gemfile.lock, Cargo.lock, poetry.lock) after running the mapped clear command. Preserves go.sum as it's needed by Go.

--- cmd/clear.go
+++ cmd/clear.go
@@ -2,10 +2,16 @@ package cmd
 
 import (
 	"fmt"
+	"os"
+	"path/filepath"
 
 	"github.com/spf13/cobra"
 	"github.com/totti-rdz/tz/internal/config"
 	"github.com/totti-rdz/tz/internal/executor"
+)
+
+var (
+	clearAllFlag bool
 )
 
 var clearCmd = &cobra.Command{
@@ -18,7 +24,8 @@ Use 'tz map clear <command>' to configure the clear command for this project.
 
 Examples:
   tz clear     # Run the configured clear command
-  tz c         # Same, using alias`,
+  tz c         # Same, using alias
+  tz c -a      # Clear + remove lock files (package-lock.json, yarn.lock, etc.)`,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		// Get current project path
 		projectPath, err := config.GetCurrentProjectPath()
@@ -38,9 +45,32 @@ Examples:
 			return fmt.Errorf("%w\n\nTip: Run 'tz map clear \"<your-clear-command>\"' to set it up", err)
 		}
 
-		// Execute the command
+		// Execute the mapped clear command
 		if err := executor.Execute(command); err != nil {
 			return err
+		}
+
+		// If -a flag is set, also remove lock files
+		if clearAllFlag {
+			lockFiles := []string{
+				"package-lock.json",
+				"yarn.lock",
+				"pnpm-lock.yaml",
+				"bun.lockb",
+				"Gemfile.lock",
+				"Cargo.lock",
+				"poetry.lock",
+			}
+
+			for _, lockFile := range lockFiles {
+				lockPath := filepath.Join(projectPath, lockFile)
+				if _, err := os.Stat(lockPath); err == nil {
+					fmt.Printf("Removing %s...\n", lockFile)
+					if err := os.Remove(lockPath); err != nil {
+						fmt.Printf("Warning: failed to remove %s: %v\n", lockFile, err)
+					}
+				}
+			}
 		}
 
 		return nil
@@ -49,4 +79,5 @@ Examples:
 
 func init() {
 	rootCmd.AddCommand(clearCmd)
+	clearCmd.Flags().BoolVarP(&clearAllFlag, "all", "a", false, "Also remove lock files (package-lock.json, yarn.lock, etc.)")
 }
